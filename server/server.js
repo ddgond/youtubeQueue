a3b3210e95b15a90a60bf2375c7909cba4075a65
Daniel Dangond <dadangond@gmail.com>
2020-02-08 23:35:35 -0500

    One more attempt at getting client ip

--- server/server.js
+++ server/server.js
@@ -109,12 +109,12 @@ io.on('connection', function(socket) {
     console.log(`a user joined room ${roomCode}`);
     connectedRoom = roomCode;
     if (!rooms[connectedRoom]) {
-      rooms[connectedRoom] = {entries: [], state: {}, users: [{id: socket.id, ip: socket.request.connection.remoteAddress}], skipVotes: []};
+      rooms[connectedRoom] = {entries: [], state: {}, users: [{id: socket.id, ip: socket.handshake.headers["x-forwarded-for"].split(",")[0]}], skipVotes: []};
     }
-    console.log(`IP: ${socket.request.connection.remoteAddress}`);
-    if (rooms[connectedRoom].users.filter((user) => user.ip === socket.request.connection.remoteAddress).length > 0) {
+    console.log(`IP: ${socket.handshake.headers["x-forwarded-for"].split(",")[0]}`);
+    if (rooms[connectedRoom].users.filter((user) => user.ip === socket.handshake.headers["x-forwarded-for"].split(",")[0]).length > 0) {
       rooms[connectedRoom].users = rooms[connectedRoom].users.map((user) => {
-        if (user.ip === socket.request.connection.remoteAddress) {
+        if (user.ip === socket.handshake.headers["x-forwarded-for"].split(",")[0]) {
           rooms[connectedRoom].entries = rooms[connectedRoom].entries.map(entry => {
             entry.votes = entry.votes.map(voter => {
               if (voter === user.id) {
@@ -135,7 +135,7 @@ io.on('connection', function(socket) {
         return user;
       });
     } else {
-      rooms[connectedRoom].users.push({id: socket.id, ip: socket.request.connection.remoteAddress});
+      rooms[connectedRoom].users.push({id: socket.id, ip: socket.handshake.headers["x-forwarded-for"].split(",")[0]});
     }
     socket.emit("statusUpdate", rooms[connectedRoom].state);
     socket.emit("queueList", rooms[connectedRoom].entries);
